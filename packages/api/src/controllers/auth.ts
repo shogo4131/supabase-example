389d3873c251da191ce4eaa3edabc9292b84c57c
shogo.fukami <shogo.fukami@route06.co.jp>
2023-07-23 23:48:52 +0900

    feat: login apiの作成

--- packages/api/src/controllers/auth.ts
+++ packages/api/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { prisma } from "../libs/prisma";
+import jwt from "jsonwebtoken";
 
 export const register = async (
   req: Request<
@@ -11,6 +12,10 @@ export const register = async (
   res: Response
 ) => {
   const { usename, email, password } = req.body;
+
+  if (!usename || !email || !password) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
 
   try {
     const hashPassword = await bcrypt.hash(password, 10);
@@ -24,6 +29,35 @@ export const register = async (
     });
 
     return res.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (
+  req: Request<never, never, { email: string; password: string }>,
+  res: Response
+) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET || "", {
+      expiresIn: "1d",
+    });
+
+    return res.json({ token });
   } catch (error) {
     console.log(error);
   }
