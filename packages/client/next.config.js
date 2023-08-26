c14ecc6a8a78c91bfb0fb5be72eaa7b457ea75be
shogo.fukami <shogo.fukami@route06.co.jp>
2023-08-26 23:55:07 +0900

    feat: serverActionsとtypedRoutesを使用できる設定の追加

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -3,6 +3,10 @@ const { withKumaUI } = require("@kuma-ui/next-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
+   experimental: {
+      typedRoutes: true,
+      serverActions: true,
+   }
 }
 
 module.exports = withKumaUI(nextConfig)
